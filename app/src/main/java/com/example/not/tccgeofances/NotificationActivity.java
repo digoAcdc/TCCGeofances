ee61d296c69050de40f04fd5707e089f6f56c436
digoacdc <barbosa_silveira@hotmail.com>
2016-11-06 12:42:47 -0200

    email

--- app/src/main/java/com/example/not/tccgeofances/NotificationActivity.java
+++ app/src/main/java/com/example/not/tccgeofances/NotificationActivity.java
@@ -1,9 +1,13 @@
 package com.example.not.tccgeofances;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
@@ -11,6 +15,7 @@ import com.synnapps.carouselview.ImageListener;
 public class NotificationActivity extends AppCompatActivity {
 
     CarouselView carouselView;
+    ImageButton imbContato;
 
     //int[] sampleImages = {R.drawable.agend, R.drawable.bannersantos, R.drawable.igesp, R.drawable.saude_integral};
     int[] sampleImages = {R.drawable.banner, R.drawable.banner1, R.drawable.banner2, R.drawable.banner3};
@@ -25,6 +30,24 @@ public class NotificationActivity extends AppCompatActivity {
         carouselView = (CarouselView) findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
         carouselView.setImageListener(imageListener);
+        imbContato = (ImageButton) findViewById(R.id.imbContato);
+
+        imbContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"Contato@nossaempresa.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "Simulação de Financiamento");
+                i.putExtra(Intent.EXTRA_TEXT   , "");
+                try {
+                    startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(NotificationActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
 
